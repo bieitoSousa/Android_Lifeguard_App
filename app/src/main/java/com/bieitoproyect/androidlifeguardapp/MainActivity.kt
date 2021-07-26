6ebe0f500f3a104df696da93a18e8a4fc5ed71a6
BIEITO_SOUSA <bieito.sousa.1991@gmail.com>
2021-07-26 23:58:45 +0200

    add content models

--- app/src/main/java/com/bieitoproyect/androidlifeguardapp/MainActivity.kt
+++ app/src/main/java/com/bieitoproyect/androidlifeguardapp/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.bieitoproyect.androidlifeguardapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.bieitoproyect.androidlifeguardapp.contentModel.CardioRespiratoria
+import com.bieitoproyect.androidlifeguardapp.contentModel.Ictus
+import com.bieitoproyect.androidlifeguardapp.contentModel.Ovance
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun bttCardioRespiratoria(view: View) {
+        val cardioRespiratoria = Intent(this, CardioRespiratoria::class.java).apply {
+        }
+        startService(cardioRespiratoria)
+    }
+
+    fun bttOvance(view: View) {  val ovance = Intent(this, Ovance::class.java).apply {
+    }
+        startService(ovance)}
+
+    fun bttIctus(view: View) {  val ictus = Intent(this, Ictus::class.java).apply {
+    }
+        startService(ictus)}
 }
---
+++ app/src/main/java/com/bieitoproyect/androidlifeguardapp/contentModel/Ovance.kt
@@ -0,0 +1,12 @@
+package com.bieitoproyect.androidlifeguardapp.contentModel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bieitoproyect.androidlifeguardapp.R
+
+class Ovance : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ovance)
+    }
+}
