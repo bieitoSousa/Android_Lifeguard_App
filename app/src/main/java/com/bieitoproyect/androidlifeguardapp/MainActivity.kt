ff3026c634ea5c0d9167360eaab567fdf3111cb8
BIEITO_SOUSA <bieito.sousa.1991@gmail.com>
2021-09-14 21:24:59 +0200

    add detail

--- app/src/main/java/com/bieitoproyect/androidlifeguardapp/MainActivity.kt
+++ app/src/main/java/com/bieitoproyect/androidlifeguardapp/MainActivity.kt
@@ -3,29 +3,57 @@ package com.bieitoproyect.androidlifeguardapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
-import com.bieitoproyect.androidlifeguardapp.contentModel.CardioRespiratoria
-import com.bieitoproyect.androidlifeguardapp.contentModel.Ictus
-import com.bieitoproyect.androidlifeguardapp.contentModel.Ovance
+import android.widget.Button
+import android.widget.Toast
+import com.bieitoproyect.androidlifeguardapp.contentModel.DetailCardioRespiratoria
+import com.bieitoproyect.androidlifeguardapp.contentModel.DetailIctus
+import com.bieitoproyect.androidlifeguardapp.contentModel.DetailOvance
+import com.google.android.material.button.MaterialButton
 
-class MainActivity : AppCompatActivity() {
+private const val TAG = "MyActivity"
+
+class MainActivity : AppCompatActivity()  {
+    private lateinit var bttCardioRespiratoria : MaterialButton
+    private lateinit var bttOvance : MaterialButton
+    private lateinit var bttIctus : MaterialButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        instance()
+        action()
     }
-
-    fun bttCardioRespiratoria(view: View) {
-        val cardioRespiratoria = Intent(this, CardioRespiratoria::class.java).apply {
+    private fun action() {
+        bttCardioRespiratoria.setOnClickListener {
+            Log.i(TAG, "Boton bttCardioRespiratoria pulsado")
+            sendMessage(" Accion Boton bttCardioRespiratoria pulsado")
+            val detailCR = Intent(this, DetailCardioRespiratoria::class.java)
+            startActivity(detailCR)
         }
-        startService(cardioRespiratoria)
+        bttOvance.setOnClickListener {
+            Log.i(TAG, "Boton bttOvance pulsado")
+            sendMessage("Accion Boton bttOvance pulsado")
+            val detailOV = Intent(this, DetailOvance::class.java)
+            startActivity(detailOV)
+        }
+        bttIctus.setOnClickListener{
+            Log.i(TAG, "Boton bttIctus pulsado")
+            sendMessage("Accion Boton bttIctus pulsado")
+            val detailIT = Intent(this, DetailIctus::class.java)
+            startActivity(detailIT)
+        }
     }
 
-    fun bttOvance(view: View) {  val ovance = Intent(this, Ovance::class.java).apply {
+    private fun instance() {
+        bttCardioRespiratoria = findViewById(R.id.bttCardioRespiratoria)
+        bttOvance = findViewById(R.id.bttOvance)
+        bttIctus = findViewById(R.id.bttIctus)
     }
-        startService(ovance)}
 
-    fun bttIctus(view: View) {  val ictus = Intent(this, Ictus::class.java).apply {
+
+    private fun sendMessage(message: String) {
+        Toast.makeText(this,  message, Toast.LENGTH_SHORT).show()
     }
-        startService(ictus)}
 }
---
+++ app/src/main/java/com/bieitoproyect/androidlifeguardapp/contentModel/DetailOvance.kt
@@ -0,0 +1,55 @@
+package com.bieitoproyect.androidlifeguardapp.contentModel
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import com.bieitoproyect.androidlifeguardapp.R
+import com.google.android.material.button.MaterialButton
+
+class DetailOvance : AppCompatActivity() {
+    private lateinit var goToIctus : MaterialButton
+    private lateinit var goToRCP : MaterialButton
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_ovance)
+        instance()
+        action()
+
+    }
+    private fun instance() {
+        var ovaceTitle : TextView = findViewById(R.id.title_ovace)
+        ovaceTitle.setText(R.string.OVACE);
+        var ovaceDesc : TextView = findViewById(R.id.tv_ovace_desc)
+        ovaceDesc.setText(R.string.rcp_desc);
+        var ovaceImage : ImageView = findViewById(R.id.iv_ovace_image)
+        ovaceImage.setImageResource(R.drawable.ovace_image)
+        goToIctus = findViewById<MaterialButton>(R.id.goToIctus)
+        goToIctus.setText(R.string.ICTUS_short)
+        goToRCP =findViewById<MaterialButton>(R.id.goToRCP)
+        goToRCP.setText(R.string.RCP_short)
+
+    }
+    private fun action() {
+        val botonsalir =findViewById<Button>(R.id.close)
+        botonsalir.setOnClickListener{
+            finish()
+        }
+
+       goToIctus =findViewById<MaterialButton>(R.id.goToIctus)
+        goToIctus.setOnClickListener{
+            val detailIT = Intent(this, DetailIctus::class.java)
+            startActivity(detailIT)
+        }
+
+
+
+        goToRCP =findViewById<MaterialButton>(R.id.goToRCP)
+        goToRCP.setOnClickListener{
+            val detailCR = Intent(this, DetailCardioRespiratoria::class.java)
+            startActivity(detailCR)
+        }
+    }
+}
